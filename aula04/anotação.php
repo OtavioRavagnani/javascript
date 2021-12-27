6e326c788ef03635b82b89527f627f5e775c6dee
Otavio Ravagnani <otavio.ravagnani01@gmail.com>
2021-12-27 15:17:52 -0300

    finalizao aula 4

---
+++ aula04/anotação.php
@@ -0,0 +1,21 @@
+regras de variaveis:
+- podem comerçar com letra, & ou_
+- não podem começar com números 
+- é possivel usar acentos e símbolos
+- não podem ter espaços 
+- não podem ser paavaras reservadas ex: var 
+
+dicas: 
+- maiúsculas e minúsculas fazem diferença
+- tente escolher nomes coerentes para as variáveis 
+- evite se tornar um programador alfabeto ex: que da o nome de variaveis de n1 n2 n3 n4 
+
+tipos de data type: comando = typeof
+- 5 , 18 ,-12 , 0.5 , -15.9 , 3.14 , 8.0 = number (infinity , NaN) 
+- "google" , 'javaScript' , `maria` = string 
+- true , false = boolean 
+- null
+- undefined 
+- object (array)
+- function
+
