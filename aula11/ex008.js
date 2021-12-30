1f07ad00afdf40248bbb513129aa2dafec919ec3
Otavio Ravagnani <otavio.ravagnani01@gmail.com>
2021-12-30 11:03:30 -0300

    re assistir

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velociadad do seu carro é ${vel}km/h`)
+if (vel >60) {
+    console.log(`Voçe ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
