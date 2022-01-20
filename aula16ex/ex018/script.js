5817d1ae424f9ff0f07fd17c8c77fc5e4282d627
Otavio Ravagnani <otavio.ravagnani01@gmail.com>
2022-01-20 20:13:50 -0300

    falta terminar

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,28 @@
+let num = document.querySelector('input#fnum')
+let lsta = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = [] 
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    }else{
+        return false
+    }
+}
+
+function isLista(n) {
+    if(i.indexOF(Number(n) != -1)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLitas(num.value, valores)) {
+        
+    } else {
+        window.alert('valor invaido ou ja encontrado na lista.')
+    }
+}
