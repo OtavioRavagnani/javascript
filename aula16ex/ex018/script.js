c2d42c1a7a16b7b49fa70737257a8378629cb40f
Otavio Ravagnani <otavio.ravagnani01@gmail.com>
2022-02-01 19:00:04 -0300

    nao finalizado

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,8 +1,28 @@
-var num = document.getElementById('fnum')
-var lista = document.getElementById('flista')
-var res = document.getElementById('res')
-var valores = []
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
-function adicionar() {
-    
+function adicionar(){
+    if(Numero(num.value) && ! Lista(num.value, valores)) {
+        window.alert('tudo ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
+
+function Numero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function Lista(n1) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
 }
