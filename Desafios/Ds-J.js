45683df02dcf33d7403f59182bc596febec48743
Otavio Ravagnani <otavio.ravagnani01@gmail.com>
2022-02-15 17:10:48 -0300

    pronto

---
+++ Desafios/Ds-J.js
@@ -0,0 +1,11 @@
+function botão1() {
+    window.alert('Você clicou no botão 1')
+}
+
+function botão2() {
+    window.alert('Você clicou no botão 2')
+}
+
+function botão3() {
+    window.alert('Você clicou no botão 3')
+}
