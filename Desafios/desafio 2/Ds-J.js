2e2b14b38f8e408cf249445125579c96b721dd88
Otavio Ravagnani <otavio.ravagnani01@gmail.com>
2022-02-15 17:36:43 -0300

    terminei

---
+++ Desafios/desafio 2/Ds-J.js
@@ -0,0 +1,5 @@
+function iniciar() {
+    let nome = window.prompt('Qual é seu nome ?')
+    let idade = window.prompt(`Olá ${nome} qual sua idade ?`)
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade !`)
+}
